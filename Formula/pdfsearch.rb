42d288107a50c4f8839141cd4eaab5cc43c4fbb7
bjorn <lxxorz@gmail.com>
2026-05-17 15:18:32 +0800

    pdfsearch: install pre-built PyMuPDF wheel via pip --target

--- Formula/pdfsearch.rb
+++ Formula/pdfsearch.rb
@@ -1,6 +1,4 @@
 class Pdfsearch < Formula
-  include Language::Python::Virtualenv
-
   desc "High-performance PDF full-text search with fzf interactive mode"
   homepage "https://github.com/0xlxx/pdfsearch"
   url "https://github.com/0xlxx/pdfsearch/archive/refs/tags/v0.1.0.tar.gz"
@@ -10,12 +8,16 @@ class Pdfsearch < Formula
 
   depends_on "python@3.13"
 
+  resource "pymupdf" do
+    url "https://files.pythonhosted.org/packages/01/89/3f8edd6c4f50ca370e2a2f2a3011face36f3760728ffe76dffec91c0fca0/pymupdf-1.27.2.3-cp310-abi3-macosx_11_0_arm64.whl"
+    sha256 "660d93cb6da5bbddf11d3982ae27745dd3a9902d9f24cdb69adab83962294b5a"
+  end
+
   def install
-    venv = virtualenv_create(libexec, "python3")
-    system libexec/"bin/pip", "install", "--upgrade", "pip"
-    system libexec/"bin/pip", "install", "PyMuPDF"
+    python3 = Formula["python@3.13"].opt_bin/"python3.13"
+    system python3, "-m", "pip", "install", "--target=#{libexec}", resource("pymupdf").cached_download
     libexec.install "pdfsearch"
-    (bin/"pdfsearch").write_env_script libexec/"pdfsearch", PATH: "#{libexec}/bin:$PATH"
+    (bin/"pdfsearch").write_env_script libexec/"pdfsearch", PYTHONPATH: libexec
   end
 
   def caveats
@@ -26,6 +28,6 @@ class Pdfsearch < Formula
   end
 
   test do
-    assert_match "pdfsearch", shell_output("#{bin}/pdfsearch --help")
+    system bin/"pdfsearch", "--help"
   end
 end
