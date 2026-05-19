bc0d2fa6b18e2423ced64dcfd2b464ce63cc8f64
bjorn <lxxorz@gmail.com>
2026-05-19 10:13:56 +0800

    Bump to v0.1.2: default backup pwd = master pwd

--- Formula/bitwarden-backup.rb
+++ Formula/bitwarden-backup.rb
@@ -3,8 +3,8 @@ class BitwardenBackup < Formula
 
   desc "Automated daily encrypted backups for Bitwarden vault"
   homepage "https://github.com/0xlxx/bitwarden-backup"
-  url "https://github.com/0xlxx/bitwarden-backup/archive/refs/tags/v0.1.1.tar.gz"
-  sha256 "28935560850b2754ea7714a079097bb91a0206879de38ee7b9281f2be64ae65a"
+  url "https://github.com/0xlxx/bitwarden-backup/archive/refs/tags/v0.1.2.tar.gz"
+  sha256 "6159d5223c96ebfb29eaaf5332e62263c5828ab61eeade98c69eb85e6a6d246e"
   license "MIT"
 
   depends_on "bitwarden-cli"
